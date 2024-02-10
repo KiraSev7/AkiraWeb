405a73f44dad1b81bc6e0b170c9eff64f6798c9c
KiraSev7 <kiratv1710@gmail.com>
2024-02-10 18:18:22 +0700

    V24.2.10

--- RANGKUMAN-MINI/javascript.js
+++
@@ -1,9 +0,0 @@
-// Deteksi perangkat
-var isAndroid = /Android/i.test(navigator.userAgent);
-
-// Jika perangkat bukan Android, tampilkan pesan alert
-if (!isAndroid) {
-    var contentDiv = document.getElementById('cvs');
-    contentDiv.style.display = 'none';
-    alert('Konten ini hanya tersedia di perangkat Android.');
-}
